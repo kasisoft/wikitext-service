da459039fc743fdb8af7b775099d4d74088fdbe4
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2017-09-15 02:57:53 +0200

    - integrated 'GenericWikiService' into 'WikiType'

--- src/main/java/com/kasisoft/cdi/services/wikitext/WikiType.java
+++ src/main/java/com/kasisoft/cdi/services/wikitext/WikiType.java
@@ -1,15 +1,40 @@
 package com.kasisoft.cdi.services.wikitext;
 
+import com.kasisoft.cdi.services.wikitext.internal.*;
+import com.kasisoft.cdi.weldex.*;
+
+import java.util.function.*;
+
+import lombok.experimental.*;
+
+import lombok.*;
+
 /**
  * @author daniel.kasmeroglu@kasisoft.net
  */
-public enum WikiType {
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public enum WikiType implements Supplier<HtmlService> {
 
-  confluence,
-  markdown,
-  mediawiki,
-  textile,
-  trac,
-  twiki;
+  confluence ( ConfluenceWikiService . class ),
+  markdown   ( MarkdownWikiService   . class ),
+  mediawiki  ( MediaWikiService      . class ),
+  textile    ( TextileWikiService    . class ),
+  trac       ( TracWikiService       . class ),
+  twiki      ( TWikiService          . class );
+  
+  Class<? extends AbstractWikiService>   clazz;
+  HtmlService                            instance;
+  
+  WikiType( Class<? extends AbstractWikiService> type ) {
+    clazz = type;
+  }
+
+  @Override
+  public synchronized HtmlService get() {
+    if( instance == null ) {
+      instance = CdiContext.component( clazz );
+    }
+    return instance;
+  }
   
 } /* ENDENUM */
